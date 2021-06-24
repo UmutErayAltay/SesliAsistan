3f360351584b52a0d602ef4f734aa98e8893564a
UmutErayAltay <83500869+UmutErayAltay@users.noreply.github.com>
2021-06-24 19:04:04 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,36 +1,10 @@
 import speech_recognition as sr
-import time
-import speech_recognition as sr
-from gtts import gTTS
-from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-import pyaudio
-import urllib.request
-import json
-import requests
-from pygame import mixer
-from gtts import gTTS
 from playsound import playsound
-instagram_adi = "nick_name"
-instagram_sifre = "sifre"
-gmail = "e-mail"
-gmail_sifre = "e-mail password"
-
-def kapama(driver):
-    döngükır=False
-    r = sr.Recognizer()
-    with sr.Microphone() as source:
-        print("kapatma dinlemedeyim")
-        audio = r.listen(source)
-    try:
-        metin = str(r.recognize_google(audio, language="tr"))
-        if(metin.find("kapat") != -1):
-            driver.close()
-            döngükır = True
-            return(döngükır)
-    except:
-        print("bir hata oldu")
-def konusma(instagram_adi, instagram_sifre, gmail, gmail_sifre):
+import smtplib
+import os
+gmail = "nickname@gmail.com"
+gmail_sifre = "password"
+def konusma(gmail, gmail_sifre):
     r = sr.Recognizer()
     with sr.Microphone() as source:
         print("dinlemedeyim")
@@ -38,39 +12,43 @@ def konusma(instagram_adi, instagram_sifre, gmail, gmail_sifre):
     try:
         metin = str(r.recognize_google(audio,language = "tr"))
         print("dediğiniz soz: " + metin)
-        if (metin.find("Instagram")!=-1):
-            driver = webdriver.Opera()
-            driver.get('https://www.instagram.com')
-            time.sleep(1)
-            username = driver.find_element_by_name("username")
-            password = driver.find_element_by_name("password")
-            username.send_keys(instagram_adi)
-            password.send_keys(instagram_sifre)
-            password.submit()
-            a=True
-            while (a):
-                c = kapama(driver)
-                if(c == True):
-                    a=False
-        elif(metin.find("posta")!=-1):
-            driver = webdriver.Chrome()
-            driver.get('https://www.gmail.com')
-            username = driver.find_element_by_name("identifier")
-            username.send_keys(gmail)
-            time.sleep(1)
-            buton = driver.find_element_by_xpath('//*[@id="identifierNext"]/div/button/span')
-            buton.click()
-            time.sleep(2)
-            password = driver.find_element_by_name("password")
-            password.send_keys(gmail_sifre)
-            buton = driver.find_element_by_xpath('//*[@id="identifierNext"]/div/button/span')
-            a = True
-            while (a):
-                c = kapama(driver)
-                if (c == True):
-                    a = False
+        if(metin.find("posta")!=-1):
+            playsound('Posta.mp3')
+            mail = smtplib.SMTP("smtp.gmail.com", 587)
+            mail.ehlo()
+            mail.starttls()
+            mail.login(gmail, gmail_sifre)
+            içerik = input("içeriği giriniz")
+            alıcı = input("alıcıyı giriniz")
+            print(alıcı)
+            print(içerik)
+            dur = input("önizleme")
+            playsound('Gonder.mp3')
+            mail.sendmail(gmail, alıcı, içerik)
+        elif(metin.find("dur")!=-1):
+            playsound('kapanis.mp3')
+            return("kapat")
+        elif(metin.find("opera")!=-1):
+            playsound('Opera.mp3')
+            os.system("start opera")
+        elif (metin.find("Google Chrome") != -1):
+            playsound('Chrome.mp3')
+            os.system("start chrome")
+        elif (metin.find("Firefox") != -1):
+            playsound('Mozilla.mp3')
+            os.system("start Firefox")
+        elif(metin.find("Instagram")!=-1):
+            playsound('instagram.mp3')
+            os.system("start www.instagram.com")
+        elif (metin.find("YouTube") != -1):
+            playsound('YouTube.mp3')
+            os.system("start www.youtube.com")
+        elif (metin.find("Google") != -1):
+            playsound('Google.mp3')
+            os.system("start www.google.com")
     except:
         print("bir hata oldu")
-while 1:
-    konusma(instagram_adi, instagram_sifre, gmail, gmail_sifre)
-
+döngü = True
+while döngü:
+    if(konusma(gmail, gmail_sifre)=="kapat"):
+        döngü=False
